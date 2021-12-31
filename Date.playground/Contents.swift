213b9af5b5ea6cd809b74eddc6e893793e727408
Emrah Turan <emrah.turan@softtech.com.tr>
2021-12-31 18:55:01 +0300

    completed project

--- Date.playground/Contents.swift
+++ Date.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+extension Date {
+    func day() -> String {
+        self.formatter(format: "dd")
+    }
+    
+    func month() -> String {
+        self.formatter(format: "MM")
+    }
+    
+    func year() -> String {
+        self.formatter(format: "yyyy")
+    }
+    
+    func dateAsPrettyString() -> String {
+        self.formatter(format: "dd<>MM<>yyyy")
+    }
+    
+    private func formatter(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
+
+
+let date: Date = Date()
+date.day()
+date.month()
+date.year()
+date.dateAsPrettyString()
